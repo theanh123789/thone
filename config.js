395a288ec8fbe21b80bc88e418e5fb224045cc1b
Thế Anh <clonetheanh1@gmail.com>
2022-12-22 14:28:59 +0700

    adhvud

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Thanh Mai';
+const giftUrl = 'https://l.facebook.com/l.php?u=https%3A%2F%2Ftheanh123789.github.io%2Fdiuagidu%2F%3Ffbclid%3DIwAR1nE2-wg2SOOoKdKvGuaWx7r6nSMoeUbrZUGeX3HjiA9uXpgoZ_YZnvkcw&h=AT0NQKAbKLZUMy1m15JDLFAIuIiOudkT31jj0_LZK6oWAkx1cldgRZzaNLMxxFJ-k2JXEBOCUoIGyqOvDksEnhM65uF5CB28OoPSIfAnQHQ8gXYTlxKhEhuX3OCq5Gva-GrKo-b-gHUlbf25_nOjwg';
+const eventName = 'Gửi em: Lại Thị';
+const titleCard = 'Tặng em ';
+const contentCard = 'Anh biết anh có nhiều khuyết điểm hơn là ưu điểm, đó là sự thiệt thòi với em. Nhưng anh hứa anh sẽ cố gắng mang đến cho em những điều tuyệt vời và chân thành nhất. Em đồng ý làm người yêu anh nhé!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'mai.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
